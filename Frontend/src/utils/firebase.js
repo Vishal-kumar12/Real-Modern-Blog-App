1a86b7715a1911ebca57d8714fb13b9fc391263a
Vishal <mannuvishal07@gmail.com>
2026-06-11 00:20:02 +0530

    frontend updated 2

--- Frontend/src/utils/firebase.js
+++ Frontend/src/utils/firebase.js
@@ -1,20 +1,6 @@
 import { initializeApp } from "firebase/app";
 import {getAuth, GoogleAuthProvider, signInWithPopup } from "firebase/auth"
 
-// const firebaseConfig = {
-// // Import the functions you need from the SDKs you need
-//   apiKey: import.meta.env.VITE_APIKEY,
-
-//   authDomain: import.meta.env.VITE_AUTHDOMAIN,
-//   projectId: import.meta.env.VITE_PROJECTID,
-//   storageBucket: import.meta.env.VITE_STORAGEBUCKET,
-//   messagingSenderId: import.meta.env.VITE_MESSAGINGSENDERID,
-//   appId: import.meta.env.VITE_APPID
-// };
-
-
-
-
 
 
 
@@ -27,15 +13,6 @@ const firebaseConfig = {
   messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
   appId: import.meta.env.VITE_FIREBASE_APP_ID
 };
-
-// const firebaseConfig = {
-//   apiKey : "AIzaSyATOjT_jyUi1abb92LATOsu2ezx6RdJJ_8",
-//   authDomain: "realmodernblogapp.firebaseapp.com",
-//   projectId: "realmodernblogapp",
-//   storageBucket: "realmodernblogapp.firebasestorage.app",
-//   messagingSenderId: "679404268936",
-//   appId: "1:679404268936:web:c126cfa2373cba2c049e3"
-// };
 
 
 // Initialize Firebase
